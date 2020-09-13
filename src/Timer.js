1ab09b9f7843c5510dc110b86be61602855ad62d
fasnimo <fasnimo@gmail.com>
2020-09-12 20:11:37 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -6,7 +6,13 @@ class Timer extends Component {
     color: "#" + Math.floor(Math.random() * 16777215).toString(16)
   };
 
-  // add your code here
+  componentDidMount(){
+    this.interval = setInterval(() => {this.clockTick()}, 1000)
+  } 
+
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
 
   render() {
     const { time, color } = this.state;
@@ -35,6 +41,7 @@ class Timer extends Component {
   handleClose = () => {
     this.props.removeTimer(this.props.id);
   };
+
 }
 
 export default Timer;
